6c221bf178ad2d9f3cee8b1f4b63aba202c31af4
velllum <velllum@yandex.ru>
2021-02-12 00:32:46 +0300

    initiall lamoda

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import json
 
 import requests
 
-url = 'https://infobit.space/api/threads/'
+# url = 'https://infobit.space/api/threads/' # получили даные из новые темы
+# url = 'https://infobit.space/api/posts/9839' # получили стаью по его ид ключь last_post_id
+# url = 'https://infobit.space/api/nodes/' # получили подкатегории из главной категории "КАТАЛОГ ИНФОПРОДУКТОВ"
+from config import key
 
-headers = {'XF-Api-Key': ''}
+url = 'https://infobit.space/api/nodes/11' # получили подкатегорию с данными из нее
+
+headers = {'XF-Api-Key': key}
 
 payload = {
-    # 'api_bypass_permissions': 1,
-    # 'key': 1
+    # "page": 20
 }
 
 r = requests.get(
@@ -21,3 +25,4 @@ r = requests.get(
 r_json_ = json.dumps(r.json(), sort_keys=True, indent=4)
 json_ = codecs.decode(r_json_, 'unicode_escape')
 print(json_)
+print(r.request.body)
